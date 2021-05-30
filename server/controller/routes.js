ef8dc06664d569883de1325f52b30806c0a4452d
Alexander Alie <alexanderalie@Alexanders-MBP.home>
2021-05-30 16:16:23 -0400

    routes.js tweak

--- server/controller/routes.js
+++ server/controller/routes.js
@@ -1,7 +1,7 @@
 /* <------------------------------------------------------------------> */
 
 var mysql = require('mysql');
-const databaseString = "mysql://root:11091991@localhost:3306/first_sql";
+const databaseString = "mysql://root:@localhost:3306/first_sql";
 const databaseConnection = mysql.createConnection(databaseString);
 databaseConnection.connect();
 
