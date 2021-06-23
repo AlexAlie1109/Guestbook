f064e6569aa9afbb66900348c32457b0cb4b9e1f
Alexander Alie <alexanderalie@alexanders-mbp.home>
2021-06-23 19:35:21 -0400

    added csv

--- server/controller/routes.js
+++ server/controller/routes.js
@@ -1,7 +1,7 @@
 /* <------------------------------------------------------------------> */
 
 var mysql = require('mysql');
-const databaseString = "mysql://root:@localhost:3306/first_sql";
+const databaseString = process.env.JAWSDB_URL || process.env.LOCAL_DATABASE;
 const databaseConnection = mysql.createConnection(databaseString);
 databaseConnection.connect();
 
